8963a0432cf5aaba2932354f4e21900c1d8662f3
TROX25 <piotrek.dziembowski@gmail.com>
2025-07-28 01:29:57 +0200

    added Psets + Props

--- propertySet_parser_1.py
+++ propertySet_parser_1.py
@@ -42,20 +42,35 @@ def main():
             #plik.write(f"Pierwszy Obiekt: {str(first_line)}\n")
             plik.write(f"Wersja Revit: {software_name[:-5]}\n\n")  # usuwam ostatnie 5 znaków (ENU)??
             
+            # Set (bez duplikatów) dla Psetów
+            psety_noDupes = set()
+            
+            # Wyszukuje Psety [ALE TYLKO TE ZACZYNAJĄCE SIE OD Pset - czyli nie wszystkie (bez np. BaseQuantities, Dimensions, Reference)]
             plik.write("Wszyskie Psety:\n")
             for pset in psets:
-                plik.write(f"{pset.Name}\n")
-                
-                if hasattr(pset, "HasProperties") and pset.HasProperties:
-                    for property in pset.HasProperties:
-                        property_name = property.Name # do tymczasowej zmiennej prop przy aktualnej iteracji przypisuje nazwe propa
-                        if hasattr(property, "NominalValue") and property.NominalValue: 
-                            value = property.NominalValue.wrappedValue
-                            plik.write(f"   - {property_name}: {value}\n")
+                if pset.Name.startswith("Pset"):
+                    if pset.Name not in psety_noDupes:
+                        psety_noDupes.add(pset.Name)
+                        plik.write(f"{pset.Name}\n")
+                        
+                        if hasattr(pset, "HasProperties") and pset.HasProperties:
+                            for property in pset.HasProperties:
+                                property_name = property.Name
+                                if hasattr(property, "NominalValue") and property.NominalValue:
+                                    content = property.NominalValue.wrappedValue
+                                    plik.write(f"   - {property_name}: {content}\n")
+                                else:
+                                    plik.write(f"   - {property_name}: -\n")
                         else:
-                            plik.write(f"   - {property_name}: -\n")
-                else:
-                    plik.write("Brak Danych !!!\n")
+                            plik.write("Brak Danych !!!\n")
+                    
+            # z Seta psety_noDupes dodaje alfabetycznie do pliku txt  
+            
+            ## NIE MUSI BYĆ PO KOLEI ALE MOŻE ##      
+            #for xxx in sorted(psety_noDupes):
+            #    plik.write(f"{xxx}\n")
+                
+                
                         
 
                     
